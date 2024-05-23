50b4a1ea9646774d7125d930fbe9e260ff0be3a3
Davide Di Francesco <davdifr@outlook.com>
2024-05-23 12:10:11 +0200

    chore: Add ngx-markdown dependency and integrate markdown editor component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarkdownEditorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'markdown-editor';
---
+++ src/app/markdown-editor/markdown-editor.component.ts
@@ -0,0 +1,62 @@
+import { I18nPluralPipe } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
+
+@Component({
+  selector: 'app-markdown-editor',
+  standalone: true,
+  imports: [FormsModule, MarkdownModule],
+  templateUrl: './markdown-editor.component.html',
+  styleUrl: './markdown-editor.component.css',
+})
+export class MarkdownEditorComponent {
+  @Input() markdown = '';
+  @Output() markdownChange = new EventEmitter<string>();
+
+  @ViewChild('markdownTextarea') textarea!: ElementRef;
+
+  selectedTab = 'editor';
+
+  addFormat(formatType: string) {
+    const textarea = this.textarea.nativeElement;
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const selectedText = this.markdown.substring(start, end);
+
+    let formattedText = '';
+    let cursorOffset = 2;
+    switch (formatType) {
+      case 'bold':
+        formattedText = `**${selectedText}**`;
+        break;
+      case 'italic':
+        formattedText = `_${selectedText}_`;
+        cursorOffset = 1; // For italic, we only have one underscore
+        break;
+      // Add more cases as needed
+    }
+
+    this.markdown =
+      this.markdown.substring(0, start) +
+      formattedText +
+      this.markdown.substring(end);
+
+    // If no text is selected, place the cursor in the same position
+    const cursorPosition = selectedText.length
+      ? start + cursorOffset + selectedText.length
+      : start + cursorOffset;
+
+    setTimeout(() => {
+      textarea.setSelectionRange(cursorPosition, cursorPosition);
+      textarea.focus();
+    }, 0);
+  }
+}
